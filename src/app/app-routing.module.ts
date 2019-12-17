50d884541e2e0ef5cf2cecf0cbae4bdf349eee2e
Gabrielly Ribeiro <gabrielly.c.ribeiro@gmail.com>
2019-12-17 15:04:25 -0300

    modificaçoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CorpoComponent } from './corpo/corpo.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { PostIdComponent } from './post-id/post-id.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'corpo', pathMatch: 'full' },
+  { path: 'corpo', component: CorpoComponent },
+  { path: 'conteudo', component: ConteudoComponent },
+  { path: 'post-id', component: PostIdComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CorpoComponent } from './corpo/corpo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { PostIdComponent } from './post-id/post-id.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    CorpoComponent,
+    RodapeComponent,
+    ConteudoComponent,
+    PostIdComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
